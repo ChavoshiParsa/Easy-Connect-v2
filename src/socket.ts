b43dccb38cc8e63aaf9bc3d41ef911007451ad39
Your Parsa <parypary82@gmail.com>
2024-03-20 01:45:20 +0330

    adding link

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = 'https://easy-connect-v2-server.onrender.com';
 
 export const socket = io(URL, {
   autoConnect: false,
